772257fce1155c5c4cfe5eb1a258b5f9396e6856
AmandaErnar <amandaernar22@gmail.com>
2026-01-22 19:44:25 -0800

    animation

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-const form = document.getElementByID('contact-form');
-
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(form);
-  const jsonData = Object.fromEntries(formData.entries());
-
-  try {
-    const response = await fetch('send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(json.Data)
-    });
-
-    if (response.ok) {
-      alert('message send successfully.');
-      form.reset();
-    } else {
-      alert('failed to send message.');
-    }
-  } catch (error) {
-    console.error('error:', error);
-    alert('an error occurred. check console!')
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    root: null,
+    threshold: 0.1
   }
-});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        console.log('Target visible:', entry.target);
+        entry.target.classList.add('seen');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+})
